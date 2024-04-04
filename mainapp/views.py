6ca5ac6292be71deee0efb1ee299b9263dc3d6cb
Abhyudaya Tiwari <130074794+FriendlyIndr@users.noreply.github.com>
2024-04-04 18:42:58 +0530

    Update views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def properties(request):
-    context = {}
+    hostels = Hostel.objects.all()
+    context = {"hostels":hostels}
     return render(request, 'main/Home.html', context) 
 
 def comparison(request):
-    context = {}
+    hostels = Hostel.objects.all()
+    context = {"hostels":hostels}
     return render(request, 'main/Comparison.html', context) 
